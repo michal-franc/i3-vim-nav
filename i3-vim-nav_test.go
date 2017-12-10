110bf378241f0f8a13e5036468319ca58b8302d7
Michal Franc <michal.franc@just-eat.com>
2017-12-10 09:11:59 +0000

    adding simpe unit tests

---
+++ i3-vim-nav_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+func TestVimRegexp(t *testing.T) {
+
+  testCases := map[string]bool{
+    "vim": true,
+    "i3-vim-nav.go (~/tools/i3-vim-nav) - vim": true,
+    "i3-vim-nav.go": false,
+  }
+
+  for key, val := range testCases {
+  actual := isItInVimContext(key)
+  expected := val
+  if actual != expected {
+    t.Errorf("For %v expected %v but got %v", key, expected, actual)
+    }
+  }
+}
